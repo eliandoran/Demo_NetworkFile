18052e518ea28fa13ff956e5380d80f01ac70780
Elian Doran <doran.adoris.elian@gmail.com>
2021-02-12 00:27:30 +0200

    List files in current directory for testing

--- server.c
+++ server.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+void ListFiles(char* path) {
+    WIN32_FIND_DATA findData;
+    HANDLE findHandle = FindFirstFile(path, &findData);
+
+    if (findHandle == INVALID_HANDLE_VALUE) {
+        // Unable to open handle for file search.
+        return;
+    }
+
+    // Iterate through each of the files.
+    do {
+        printf("%s\n", findData.cFileName);
+    } while (FindNextFile(findHandle, &findData) != 0);
+}
 
 int main() {
-    printf("Hello world.\n");
+    ListFiles("./*");
     return EXIT_SUCCESS;
 }

1c34ae80df2207799167b261aae96084d614869a
Elian Doran <doran.adoris.elian@gmail.com>
2021-02-13 19:37:10 +0200

    Calculate body size for ls

--- server/ls.h
+++ server/ls.h
@@ -10,16 +10,22 @@ int NetworkSend_ListFiles(SOCKET clientSocket, char* path) {
     struct NetworkSend_Response response;
     WIN32_FIND_DATAA findData;
     HANDLE findHandle = FindFirstFileA(path, &findData);
+    int bodySize = 0;
 
     if (findHandle != INVALID_HANDLE_VALUE) {
-        // Iterate through each of the files.
+        // Iterate through each of the files, to determine the body size.
         do {
             // Ignore directories.
             if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
                 continue;
 
             printf("Got file: %s\n", findData.cFileName);
+            bodySize += strlen(findData.cFileName + 1);
         } while (FindNextFileA(findHandle, &findData) != 0);
+
+        printf("Body size: %d\n", bodySize);
+
+        // Generate the response body by iterating again through
 
         response.status = NETWORKSEND_RESPONSE_STATUS_OK;
     } else {

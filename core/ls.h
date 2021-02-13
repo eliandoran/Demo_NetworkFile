b71ca29dbfc9e30a0017f3ef150c049b4fbb41ef
Elian Doran <doran.adoris.elian@gmail.com>
2021-02-13 20:21:39 +0200

    Send file listing from server to client

--- core/ls.h
+++ core/ls.h
@@ -6,11 +6,27 @@
 #include "socket.h"
 #include "response.h"
 
+struct NetworkSend_FileListing {
+    char* name;
+};
+
+int NetworkSend_SendFileListing(SOCKET socket, struct NetworkSend_FileListing *fileData) {
+    int nameLength = strlen(fileData->name);
+    int bufLength = nameLength + 1; // incl. null terminator.
+    char *buf = (char*)malloc(sizeof(char) * bufLength);
+    buf[nameLength + 1] = '\0';
+    strncpy(buf, fileData->name, nameLength);
+
+    int result = Socket_Send(socket, buf, bufLength);
+    free(buf);
+    return result;
+}
+
 int NetworkSend_ListFiles(SOCKET clientSocket, char* path) {
     struct NetworkSend_Response response;
+    struct NetworkSend_FileListing fileData;
     WIN32_FIND_DATAA findData;
     HANDLE findHandle = FindFirstFileA(path, &findData);
-    int bodySize = 0;
 
     if (findHandle != INVALID_HANDLE_VALUE) {
         // Iterate through each of the files, to determine the body size.
@@ -20,12 +36,11 @@ int NetworkSend_ListFiles(SOCKET clientSocket, char* path) {
                 continue;
 
             printf("Got file: %s\n", findData.cFileName);
-            bodySize += strlen(findData.cFileName + 1);
+
+            fileData.name = findData.cFileName;
+            printf("%s\n", fileData.name);
+            NetworkSend_SendFileListing(clientSocket, &fileData);
         } while (FindNextFileA(findHandle, &findData) != 0);
-
-        printf("Body size: %d\n", bodySize);
-
-        // Generate the response body by iterating again through
 
         response.status = NETWORKSEND_RESPONSE_STATUS_OK;
     } else {
